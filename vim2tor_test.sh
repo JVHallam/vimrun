0e68df6891dac1f2c33c786e5213f83035d31ab1
Jake Hallam <jakehallam95@gmail.com>
2019-06-20 18:47:20 +0100

    House keeping

--- vim2tor_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filename="/tmp/vim2tor$RANDOM"
-secondFileName="/tmp/vim2tor_second$RANDOM";
-cp base_file_vim2tor.txt $filename;
-cp second_tab_base_file.txt $secondFileName; 
-vi $filename $secondFileName -c "tab all";
