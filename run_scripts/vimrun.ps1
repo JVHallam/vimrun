b759ad0bf7ac1f5422dc356f308df3302a506d3e
Jake Hallam <jakehallam95@gmail.com>
2020-04-15 15:45:42 +0100

    Uses temp directory now

--- run_scripts/vimrun.ps1
+++ run_scripts/vimrun.ps1
@@ -1,3 +1,4 @@
-$TempDirectory = "test"
+$TempDirectory = "$($env:TEMP)\vimrun"
+mkdir $TempDirectory;
 vim resources/* "+tab all" "+tabdo exec 'write' '$($TempDirectory)/' . expand('%:t')" "+qall";
 vim "$($TempDirectory)/*" "+tab all";
