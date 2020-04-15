d639b9444de54339546db0797b0b337f1886d84c
Jake Hallam <jakehallam95@gmail.com>
2020-04-15 12:11:15 +0100

    Cleaned up the ps1 file

--- run_scripts/vimrun.ps1
+++ run_scripts/vimrun.ps1
@@ -1,21 +1,3 @@
 $TempDirectory = "test"
-# ls resources | foreach -process { cat "resources/$($_.Name)" > "$($TempDirectory)/$($_.Name)" };
-# vim "$($TempDirectory)/*" -c "tab all";
-
-vim resources/* "+tab all" "+tabdo exec 'write' 'test/' . expand('%:t')" "+qall";
-vim test/* "+tab all";
-
-<#
-## What this script should do
-* Open all the resources files
-* Save them to a new directory
-	* Grab the files name
-	* Save that file to a new directory
-* close the original files
-* Open the copies
-
-OR
-
-* Just make sure that the user is editing the copies instead of the originals
-
-#>
+vim resources/* "+tab all" "+tabdo exec 'write' '$($TempDirectory)/' . expand('%:t')" "+qall";
+vim "$($TempDirectory)/*" "+tab all";
