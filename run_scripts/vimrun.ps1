ba82bb21b246dc5abe2547f347e9a49f6a9837e8
Jake Hallam <jakehallam95@gmail.com>
2020-04-16 11:20:56 +0100

    Handled an error message

--- run_scripts/vimrun.ps1
+++ run_scripts/vimrun.ps1
@@ -1,4 +1,6 @@
 $TempDirectory = "$($env:TEMP)\vimrun"
-mkdir $TempDirectory;
+if( !(Test-Path $TempDirectory) ){
+	mkdir $TempDirectory;
+}
 vim resources/* "+tab all" "+tabdo exec 'write' '$($TempDirectory)/' . expand('%:t')" "+qall";
 vim "$($TempDirectory)/*" "+tab all";
