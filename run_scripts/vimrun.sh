43b7c9bc1cc44dc9b3f1bd0d427ce9fac496196f
jake hallam <jakehallam95@gmail.com>
2020-05-02 16:28:46 +0100

    removed duplicates

--- run_scripts/vimrun.sh
+++ run_scripts/vimrun.sh
@@ -2,6 +2,6 @@
 
 dirname="/tmp/vimrun$RANDOM"
 mkdir $dirname;
-vim output.txt -c "source resources/tabbed_allthings.vimscript"
-mv output.txt $dirname
+vim -c "source resources/tabbed_allthings.vimscript"
+mv *.vimrun $dirname
 time vim $dirname/* -c "tab all";
